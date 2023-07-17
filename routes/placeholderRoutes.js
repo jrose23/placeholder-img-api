add4f03fb1e3f63082db38076c3df88f440533ad
jrose23 <rosejustinb@gmail.com>
2023-07-17 18:17:57 -0400

    Added HSL color placeholders

--- routes/placeholderRoutes.js
+++ routes/placeholderRoutes.js
@@ -5,6 +5,7 @@ const {
     getNamedColorPlaceholder,
     getHexColorPlaceholder,
     getRgbColorPlaceholder,
+    getHslColorPlaceholder,
     getRandomColorPlaceholder
 } = require('../controllers/placeholderControllers');
 
@@ -14,11 +15,14 @@ router.get('/:width/:height', getGenericPlaceholder);
 // Get placeholder with named color
 router.get('/:width/:height/color/name/:colorName', getNamedColorPlaceholder);
 
-// Get placeholder with hex color
+// Get placeholder with hex color (without # symbol)
 router.get('/:width/:height/color/hex/:hexCode', getHexColorPlaceholder);
 
 // Get placeholder with RGB color
 router.get('/:width/:height/color/rgb/:red/:green/:blue', getRgbColorPlaceholder);
+
+// Get placeholder with HSL color (without % symbol)
+router.get('/:width/:height/color/hsl/:hue/:saturation/:lightness', getHslColorPlaceholder);
 
 // Get placeholder with random color
 router.get('/:width/:height/color/random', getRandomColorPlaceholder);
